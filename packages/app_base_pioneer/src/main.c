9233659570aaa342424cb6d38126359db67bfe15
Felipe Bombardelli <felipebombardelli@gmail.com>
2026-06-08 21:07:00 -0300

    versao 0.0.1 : adicionados primeiros pacotes

---
+++ packages/app_base_pioneer/src/main.c
@@ -0,0 +1,99 @@
+/**
+ * Pioneer-P3DX communication library
+ *  Copyright (C) 2024  Visao Robotica e Imagem (VRI)
+ *   - Felipe Bombardelli <felipebombardelli@gmail.com>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * * */
+
+// ============================================================================
+//  Header
+// ============================================================================
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ufr.h>
+#include <signal.h>
+#include <math.h>
+
+#include "pioneer_p3dx.h"
+
+#ifndef ROBOT_TOPIC_CMD_VEL
+#define ROBOT_TOPIC_CMD_VEL "@new ros_humble:topic @coder ros_humble:twist @topic /cmd_vel @debug 0"
+#endif
+
+#ifndef ROBOT_TOPIC_ODOM
+#define ROBOT_TOPIC_ODOM "@new ros_humble @coder ros_humble:pose @debug 0 @topic /odom"
+#endif
+
+// ============================================================================
+//  Main Pioneer
+// ============================================================================
+
+int main(int argc, char *argv[]) {
+    link_t timer = ufr_subscriber("@new timer @time 200ms");
+    link_t vel_cmd = ufr_subscriber("@new mqtt @coder msgpack @host 177.153.62.174 @topic /cmd_vel");
+    link_t odom = ufr_publisher("@new mqtt @coder msgpack  @host 177.153.62.174 @topic /odom");
+
+    pioneer_connect("/dev/ttyUSB0", 0);
+    pioneer_disable_sonars();
+    // pioneer_enable_motors();
+
+    // Main loop
+    int count = 0;   
+    int16_t i16_vel = 0;
+    int16_t i16_rotvel = 0;
+    float vel=0, rotvel=0;
+    while( ufr_loop_ok() ) {
+
+        if ( ufr_recv_async(&vel_cmd) == UFR_OK ) {
+            ufr_get(&vel_cmd, "%f %f", &vel, &rotvel);
+            i16_vel = (int16_t) (vel * 100.0);
+            i16_rotvel = (int16_t) rotvel;
+            pioneer_vel( i16_vel );
+            pioneer_rotvel( i16_rotvel );
+            printf("[LOG]: %d %d\n", i16_vel, i16_rotvel);
+            count = 0;
+        }
+
+        if ( ufr_recv_async(&timer) == UFR_OK ) {
+            if ( count >= 3 ) {
+                // vel = 0;
+                // rotvel = 0;
+                pioneer_vel(i16_vel);
+                pioneer_rotvel(i16_rotvel);
+                printf("[LOG]: %d %d\n", i16_vel, i16_rotvel);
+                count = 0;
+                // printf("[LOG]: Timeout\n");
+            }
+            count += 1;
+        }
+
+        // publishes the odometry
+        int pos_x, pos_y;
+        float pos_th;
+        pioneer_read(&pos_x, &pos_y, &pos_th);
+        ufr_put(&odom, "%d %d %f %f %f\n", pos_x, pos_y, pos_th*M_PI/180.0, vel, rotvel);
+
+        // wait 50ms
+        usleep(50000);
+    }
+
+    // End
+    pioneer_close();
+    return 0;
+}
