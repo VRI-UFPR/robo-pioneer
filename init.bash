ebbebd169731d22ec041ef1aa17c529e5cd276a8
Felipe Bombardelli <felipebombardelli@gmail.com>
2026-06-28 21:38:02 -0300

    testando o ros2

--- init.bash
+++ init.bash
@@ -1,6 +1,12 @@
-host="vriufpr.ddns.net"
+# MQTT
+# host="vriufpr.ddns.net"
+# export UFR_CMDVEL="@new mqtt @coder msgpack @host $host @topic /pioneer/cmd_vel"
+# export UFR_SCAN="@new mqtt @coder msgpack @host $host @topic /pioneer/scan"
+# export UFR_ODOM="@new mqtt @coder msgpack @host $host @topic /pioneer/odom"
+# export UFR_CAMERA="@new video @@new mqtt @@coder msgpack @@host $host @@topic /pioneer/camera"
 
-export UFR_CMDVEL="@new mqtt @coder msgpack @host $host @topic /pioneer/cmd_vel"
-export UFR_SCAN="@new mqtt @coder msgpack @host $host @topic /pioneer/scan"
-export UFR_ODOM="@new mqtt @coder msgpack @host $host @topic /pioneer/odom"
-# export UFR_CAMERA  = "@new video @@new mqtt @@coder msgpack @@host $host @@topic /pioneer/camera"
+
+# ROS 2
+export UFR_CMDVEL="@new ros2 @coder ros2:twist @topic /cmd_vel"
+export UFR_SCAN="@new ros2 @coder ros2:lidar @topic /pioneer/scan"
+export UFR_ODOM="@new ros2 @coder ros2:tf"
